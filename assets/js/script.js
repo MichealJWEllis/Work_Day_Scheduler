f7eacff23bdae059e9886f8a0a8fec896f098c51
michealjwellis <mellis.eriecon@gmail.com>
2021-03-11 14:25:39 -0500

    finished basic devlop / added css,js,html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // Display current date 
 const yourDate = moment().format("MMMM Do YYYY");
 const displayDate = document.getElementById('currentDay')
-const displayTime = document.getElementById('currentTime')
+
 displayDate.innerHTML = yourDate;
 
 // Will reload current time on HTML
 setInterval(function constTime() {
+  const displayTime = document.getElementById('currentTime')
   const yourTime = moment().format("h:mm:ss A")
   displayTime.innerHTML = yourTime;
 

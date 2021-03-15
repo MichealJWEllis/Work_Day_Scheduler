df24e1119a36b800ee7ddc2dc42d99fb0c8e071d
michealjwellis <mellis.eriecon@gmail.com>
2021-03-15 16:30:48 -0400

    small changes and js editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,31 @@ setInterval(function constTime() {
   displayTime.innerHTML = yourTime;
 
 }, 1000)
+
+// this will color the teaxtarea based on the timeline where the colors 1-3 are defined as the following:-
+// 1. grey equals past the hour
+// 2. red equals current hour
+// 3. green eqauls future hours
+function updateByTheHour() {
+  var currentHour = moment().hour("h:mm A");
+  var scheduleTime = getElementById("#timeSlot");
+  console.log(scheduleTime);
+  console.log(currentHour);
+  // $(".time-block").each(function () {
+  //     var blockHour = parseInt($(this).attr("id").split(" ")[0]);
+
+  //     if (blockHour < currentHour) {
+  //         $(this).addClass("past");
+  //     } else if (blockHour === currentHour) {
+  //         $(this).removeClass("past");
+  //         $(this).addClass("present");
+  //     } else {
+  //         $(this).removeClass("past");
+  //         $(this).removeClass("present");
+  //         $(this).addClass("future");
+  //     }
+  // });
+}
+//this calls the function updateByTheHour
+updateByTheHour();
+
